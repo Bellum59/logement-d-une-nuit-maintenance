17964aa2adc7e5e0061f96e77114270b30e8d496
bellum59 <clement.vanbellingen@yahoo.fr>
2024-02-26 13:08:43 -0500

    #2 table version 2

--- serveur/table.sql
+++ serveur/table.sql
@@ -1,8 +1,12 @@
 CREATE TABLE `reservation`(
-    `idReservation` INT(4),
+    `idReservation` INT(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `idLogement` INT(4),
     `dateDebutReservation` DATE,
     `dateFinReservation` DATE,
     `nomReservation` VARCHAR(255),
     `prenomReservation` VARCHAR(255),
     `emailReservation` VARCHAR(255)
 )
+
+ALTER TABLE `reservation`
+ADD FOREIGN KEY (`idLogement`) REFERENCES `logement`(`id`);
