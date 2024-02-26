330ec6ebaa23b3512eaf5fb24b158e16091b857d
bellum59 <clement.vanbellingen@yahoo.fr>
2024-02-26 13:01:49 -0500

    #2 premiere version de la table

---
+++ serveur/table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `reservation`(
+    `idReservation` INT(4),
+    `dateDebutReservation` DATE,
+    `dateFinReservation` DATE,
+    `nomReservation` VARCHAR(255),
+    `prenomReservation` VARCHAR(255),
+    `emailReservation` VARCHAR(255)
+)
